ea5d73579de7781e6790ce3154c9b075e357871d
Jeremy Jon Cruz Montano <jjcmontano@gmail.com>
2023-05-04 14:10:40 +1000

    Add SummaryController

---
+++ ReportSummary/ReportSummary.Api/Controllers/SummaryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReportSummary.Services;
+
+namespace ReportSummary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SummaryController : ControllerBase
+    {
+        private readonly ISummaryService _summaryService;
+
+        public SummaryController(ISummaryService summaryService)
+        {
+            _summaryService = summaryService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get([FromRoute] Guid id)
+        {
+            var summaryResult = await _summaryService.GetReportSummaryAsync(id);
+
+            if (summaryResult != null)
+            {
+                return Ok(summaryResult);
+            }
+
+            return NoContent();
+        }
+    }
+}
